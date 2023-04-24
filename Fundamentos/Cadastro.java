2ed762197d6b29aadfec5a8954f1123ff934e3ae
juliodemarchi <juliodemarc@outlook.com>
2023-04-23 23:46:44 -0400

    origin

---
+++ Fundamentos/Cadastro.java
@@ -0,0 +1,22 @@
+package io.github.jiangdequan;
+
+import javax.sql.rowset.spi.SyncResolver;
+
+public class Cadastro {
+    public static void main(String[] args) {
+
+        var a = "Nome: Rogerio Joaquim Barbosa";
+        System.out.println(a);
+        // Nome
+
+        var b = "Salario: 24.800,58";
+        System.out.println(b);
+        // Salario
+
+        var c = "CPF: 458 123 563 47";
+        System.out.println(c);
+        // CPF
+
+    }
+
+}
