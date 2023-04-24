7ffe58041437bc95e29b9e2967d05bf38cf36ce7
juliodemarchi <juliodemarc@outlook.com>
2023-04-24 19:48:34 -0400

    origin

--- Fundamentos/Cadastro.java
+++ Fundamentos/Cadastro.java
@@ -5,17 +5,35 @@ import javax.sql.rowset.spi.SyncResolver;
 public class Cadastro {
     public static void main(String[] args) {
 
-        var a = "Nome: Rogerio Joaquim Barbosa";
-        System.out.println(a);
+        var a = "Nome: ";
+        var Nome = "Antonio Teodoro";
+        System.out.println(a + Nome);
         // Nome
 
-        var b = "Salario: 24.800,58";
-        System.out.println(b);
+        var c = "CPF: ";
+        var CPF = "256.236.258.78";
+        System.out.println(c + CPF);
+        // CPF
+
+        var d = "RG: ";
+        var RG = "458956";
+        System.out.println(d + RG);
+        // RG
+
+        var e = "Data de nascimento: ";
+        var Datadenascimento = "25/04/2001";
+        System.out.println(e + Datadenascimento);
+
+        var b = "Salario: ";
+        double Salario = 23568.89;
+        System.out.println(b + Salario);
         // Salario
 
-        var c = "CPF: 458 123 563 47";
-        System.out.println(c);
-        // CPF
+        var s = "Saldo disponivel: R$";
+        double Saldo;
+        Saldo = 1203.65;
+        System.out.println(s + Saldo);
+        // Saldo
 
     }
 
