103d4b3ced5328dffcc5e6d4665e17d2c78021ab
juliodemarchi <juliodemarc@outlook.com>
2023-04-25 21:05:19 -0400

    main

--- Fundamentos/NocaoPonto.java
+++ Fundamentos/NocaoPonto.java
@@ -1,5 +1,27 @@
 package io.github.jiangdequan;
 
-public class NocaoDePontos {
-    
+public class NocaoPonto {
+
+    public static void main(String[] args) {
+
+        String s = "Bom dia X";
+        s = s.replace("X", "Senhora! Tudo bem? Isso e um assalto");
+        s = s.toUpperCase();
+        s = s.concat("!!!");
+
+        System.out.println(s);
+
+        String x = "Leo".toUpperCase();
+        System.out.println(x);
+
+        String y = "Bom dia X".replace("X", "Gui").toUpperCase().concat("!!!");
+        System.out.println(y);
+        // string pode ser repetida
+
+        // Tipos primitivos não tem o operador "."
+        int a = 3;
+        System.out.println(a);
+
+    }
+
 }
