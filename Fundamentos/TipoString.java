4d83dbefe3f14711cf7e3bab3b2815b85cb43635
juliodemarchi <juliodemarc@outlook.com>
2023-04-27 20:38:27 -0400

    main

---
+++ Fundamentos/TipoString.java
@@ -0,0 +1,11 @@
+package io.github.jiangdequan;
+
+public class TipoString {
+    public static void main(String[] args) {
+        System.out.println("Ola pessoal".charAt(2));
+
+        String s = "Boa tarde";
+        System.out.println(");
+
+    }
+}
