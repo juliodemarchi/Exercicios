869c58ab9beb3f9780e4550aa21c02c8dbbebe0b
juliodemarchi <juliodemarc@outlook.com>
2023-04-27 20:57:20 -0400

    main

--- Fundamentos/TipoString.java
+++ Fundamentos/TipoString.java
@@ -2,10 +2,24 @@ package io.github.jiangdequan;
 
 public class TipoString {
     public static void main(String[] args) {
-        System.out.println("Ola pessoal".charAt(2));
+        System.out.println("Olá pessoal".charAt(2));
 
         String s = "Boa tarde";
-        System.out.println(");
+        System.out.println(s.concat("!!!"));
+        System.out.println(s + "!!!");
+        System.out.println(s.startsWith("Boa"));
+        System.out.println(s.toLorweCase().startsWith("boa"));
+        System.out.println(s.toUpperCase().endsWith("TARDE"));
+        System.out.println(s.length());
+        System.out.println(s.toLowerCase().equals("boa tarde"));
+        System.out.println(s.equalsIgnoreCase("boa tarde"));
+
+        var nome = "Pedro";
+        var sobrenome = "Santos";
+        var idade = 33;
+        var salario = 12345.987;
+
+        System.out.println("Nome: " + nome + " Sobrenome: " + sobrenome);
 
     }
 }
