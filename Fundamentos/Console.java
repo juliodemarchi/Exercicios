7b1c8fd36d7cdd9c4c50fc0004037c890e888e1a
juliodemarchi <juliodemarc@outlook.com>
2023-05-03 19:32:34 -0400

    main

---
+++ Fundamentos/Console.java
@@ -0,0 +1,17 @@
+package io.github.jiangdequan;
+
+public class Console {
+
+    public static void main(String[] args) {
+
+        System.out.println("Bom");
+        System.out.println(" dia!\n\n");
+
+        System.out.println("Como foi?");
+        System.out.println("como é?");
+
+        System.out.printf("Megasena: %d %d %d %d %d %d", 1, 2, 3, 4, 5, 6);
+        System.out.printf(" Salario: %.1f", 1234.5678);
+
+    }
+}
