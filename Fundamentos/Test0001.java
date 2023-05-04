fec4322601c28be896e107cabdad3b56e9c563cb
juliodemarchi <juliodemarc@outlook.com>
2023-05-03 20:10:33 -0400

    main

---
+++ Fundamentos/Test0001.java
@@ -0,0 +1,16 @@
+package io.github.jiangdequan;
+
+public class Test0001 {
+    public static void main(String[] args) {
+
+        int numero = -10;
+        String resultado = "";
+        if (numero >= 0) {
+            resultado = "positivo";
+        } else {
+            resultado = "negativo";
+        }
+        System.out.println("Hello, World! O numero e " + resultado);
+
+    }
+}
