30efb9020f6f5398b0f9c4eb9e255f3fdf834f55
juliodemarchi <juliodemarc@outlook.com>
2023-05-07 08:19:09 -0400

    main

--- Fundamentos/TiposPrimitivosNumerico.java
+++ Fundamentos/TiposPrimitivosNumerico.java
@@ -4,6 +4,20 @@ public class TiposPrimitivosNumeros {
 
     public static void main(String[] args) {
 
+        double a = 1; // implicita
+        System.out.println(a);
+
+        float b = (float) 1.98736478; // explicita ST
+        System.out.println(b);
+
+        int c = 4;
+        byte d = c;
+        System.out.println(d);
+
+        double e = 1.9999999;
+        int f = (int) e;
+        System.out.println(f);
+
     }
 
 }
